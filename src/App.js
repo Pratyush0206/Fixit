3ba0889c294ed2ee00cda392ff4a1dd0c42ffe74
Pratyush <pratyushprasad80797@gmail.com>
2026-06-28 11:57:06 +0530

    add interactive map with Leaflet

--- src/App.js
+++ src/App.js
@@ -156,6 +156,29 @@ const generateEscalationLetter = async (issue) => {
         </div>
       </div>
 
+      {/* Map */}
+      <div className="max-w-2xl mx-auto px-4 mb-6">
+        <div className="rounded-xl overflow-hidden border border-gray-800" style={{height: '300px'}}>
+          <MapContainer center={[20.5937, 78.9629]} zoom={5} style={{height: '100%', width: '100%'}}>
+            <TileLayer
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              attribution='&copy; OpenStreetMap contributors'
+            />
+            {issues.map(issue => (
+              issue.lat && issue.lng && (
+                <Marker key={issue.id} position={[issue.lat, issue.lng]}>
+                  <Popup>
+                    <b>{issue.category}</b><br/>
+                    {issue.location}<br/>
+                    Severity: {issue.severity}
+                  </Popup>
+                </Marker>
+              )
+            ))}
+          </MapContainer>
+        </div>
+      </div>
+
       <div className="max-w-2xl mx-auto p-4">
         {/* Report Form */}
         <div className="bg-gray-900 rounded-xl p-6 mb-6 border border-gray-800">
